c24a437c389daed3d2504d00bc094088bbb2783b
Booker King <btking1@my.waketech.edu>
2022-05-08 17:05:13 -0400

    add javascript to create functionality for pass gen

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -12,6 +9,39 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//Start function
+function generatePassword(){
+  var lowercase= "abcdefghijklmnopqrstuvxyz" 
+  var uppercase= lowercase.toUpperCase()
+  var numbers= "1234567890"
+  var special= "!@#$^%&*"
+  var characters= "" 
+  while(characters=== ""){
+    if(confirm("Include lowercase?")){
+      characters+=lowercase
+    }
+    if(confirm("Include uppercase?")){
+      characters+=uppercase
+    }
+    if(confirm("Include number?")){
+      characters+=numbers
+    }
+    if(confirm("Include special characters?")){
+      characters+=special
+    }
+  }
+  var password= ""
+  var numberofcharacters= 0
+  //loop for random selector 
+  while(numberofcharacters<8 || numberofcharacters>128){
+    numberofcharacters=Number(confirm("number of characters? Choose 8 to 128"))
+  }
+  for(var i=0; i<numberofcharacters; i++){
+    var index= Math.floor(Math.random()*characters.length)
+    password += characters[index]
+  }
+  return password
+}
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
